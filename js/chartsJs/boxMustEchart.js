f6118a5ad1a98e9143a6fec42c9aa32310dc458b
William <1072231718@qq.com>
2020-11-09 17:52:37 +0800

    盒须图

--- js/chartsJs/boxMustEchart.js
+++ js/chartsJs/boxMustEchart.js
@@ -1,18 +1,15 @@
+// 盒须图类，仅仅继承普通图表的方法
+class BoxMustEchart extends Echarts {
+  constructor(opt, fn) {
+    super(opt, fn);
+    this.setOption();
+  }
+}
+
+
 //盒须图
-let boxMust = [
-  [850, 740, 900, 1070, 930, 850, 950, 980, 980, 880, 1000, 980, 930, 650, 760, 810, 1000, 1000, 960, 960],
-  [960, 940, 960, 940, 880, 800, 850, 880, 900, 840, 830, 790, 810, 880, 880, 830, 800, 790, 760, 800],
-  [880, 880, 880, 860, 720, 720, 620, 860, 970, 950, 880, 910, 850, 870, 840, 840, 850, 840, 840, 840],
-  [890, 810, 810, 820, 800, 770, 760, 740, 750, 760, 910, 920, 890, 860, 880, 720, 840, 850, 850, 780],
-  [890, 840, 780, 810, 760, 810, 790, 810, 820, 850, 870, 870, 810, 740, 810, 940, 950, 800, 810, 870]
-];
-
-var boxMustdata = echarts.dataTool.prepareBoxplotData(boxMust, {
-  layout: 'vertical'
-});
-
-
-let boxMustFirstOption = {
+let boxMustOption = () => {
+  const option = {
     tooltip: {
       trigger: 'item',
       axisPointer: {
@@ -21,17 +18,15 @@ let boxMustFirstOption = {
     },
     grid: {
       left: '13%',
-      top:'10%',
+      top: '10%',
       right: '10%',
       bottom: '10%'
     },
-  color:colorList,
-
-  yAxis: {
+    // color: colorList,
+    yAxis: {
       type: 'category',
-      data: boxMustdata.axisData,
+      // data: [],
       boundaryGap: true,
-      nameGap: 30,
       splitArea: {
         show: false
       },
@@ -44,7 +39,7 @@ let boxMustFirstOption = {
     },
     xAxis: {
       type: 'value',
-      name: 'km/s minus 299,000',
+      name: '',
       splitArea: {
         show: false,
       }
@@ -53,7 +48,7 @@ let boxMustFirstOption = {
       {
         name: 'boxplot',
         type: 'boxplot',
-        data: boxMustdata.boxData,
+        data: [],
         tooltip: {
           formatter: function (param) {
             return [
@@ -70,85 +65,117 @@ let boxMustFirstOption = {
       {
         name: 'outlier',
         type: 'scatter',
-        data: boxMustdata.outliers
+        data: []
       }
     ]
   };
+  return option;
+}
+
+let boxMustSecondOption = ()=>{
+  const option = {
+    tooltip: {
+      trigger: 'item',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    grid: {
+      left: '13%',
+      top: '12%',
+      right: '10%',
+      bottom: '10%'
+    },
+    xAxis: {
+      type: 'category',
+      data: [],
+      boundaryGap: true,
+      splitArea: {
+        show: false
+      },
+      axisLabel: {
+        formatter: 'expr {value}'
+      },
+      splitLine: {
+        show: false
+      }
+    },
+    color: colorList,
+    yAxis: {
+      type: 'value',
+      name: '',
+      nameTextStyle: {
+        align: 'left',
+        padding: [0, 0, -10, 0]
+      },
+      splitArea: {
+        show: false
+      }
+    },
+    series: [
+      {
+        name: 'boxplot',
+        type: 'boxplot',
+        data: [],
+        tooltip: {
+          formatter: function (param) {
+            return [
+              'Experiment ' + param.name + ': ',
+              'upper: ' + param.data[5],
+              'Q3: ' + param.data[4],
+              'median: ' + param.data[3],
+              'Q1: ' + param.data[2],
+              'lower: ' + param.data[1]
+            ].join('<br/>');
+          }
+        }
+      },
+      {
+        name: 'outlier',
+        type: 'scatter',
+        data: []
+      }
+    ]
+  };
+  return option;
+}
+
+//盒须图
+let boxMust = [
+  [850, 740, 900, 1070, 930, 850, 950, 980, 980, 880, 1000, 980, 930, 650, 760, 810, 1000, 1000, 960, 960],
+  [960, 940, 960, 940, 880, 800, 850, 880, 900, 840, 830, 790, 810, 880, 880, 830, 800, 790, 760, 800],
+  [880, 880, 880, 860, 720, 720, 620, 860, 970, 950, 880, 910, 850, 870, 840, 840, 850, 840, 840, 840],
+  [890, 810, 810, 820, 800, 770, 760, 740, 750, 760, 910, 920, 890, 860, 880, 720, 840, 850, 850, 780],
+  [890, 840, 780, 810, 760, 810, 790, 810, 820, 850, 870, 870, 810, 740, 810, 940, 950, 800, 810, 870]
+];
+var boxMustdata = echarts.dataTool.prepareBoxplotData(boxMust, {
+  layout: 'vertical',//水平
+});
+let boxMustOneData = {
+  id: 'boxMustFirst',
+  yAxis: boxMustdata.axisData,
+  config: {
+    "xAxis.name": 'km/s minus 299,000',
+  },
+  seriesData: [{name: "boxplot", type: "boxplot", data: boxMustdata.boxData},
+    {name: 'outlier', type: 'scatter', data: boxMustdata.outliers}],
+}
+let boxMustFirst = new BoxMustEchart(GetOpiton(boxMustOneData, boxMustOption()));
+// boxMustFirst.showOption();
 
 
-
-var boxMustOne = document.getElementById('boxMustFirst');
-var boxMustFirst = echarts.init(boxMustOne);
-boxMustFirst.setOption(boxMustFirstOption);
 
 //盒须图2
 var boxMustdata2 = echarts.dataTool.prepareBoxplotData(boxMust);
-let boxMustSecondOption  = {
-  tooltip: {
-    trigger: 'item',
-    axisPointer: {
-      type: 'shadow'
-    }
+let boxMustTwoData = {
+  id: 'boxMustSecond',
+  xAxis: boxMustdata2.axisData,
+  config: {
+    "yAxis.name": 'km/s minus 299,000',
   },
-  grid: {
-    left: '13%',
-    top:'12%',
-    right: '10%',
-    bottom: '10%'
-  },
-  xAxis: {
-    type: 'category',
-    data: boxMustdata2.axisData,
-    boundaryGap: true,
-    nameGap: 30,
-    splitArea: {
-      show: false
-    },
-    axisLabel: {
-      formatter: 'expr {value}'
-    },
-    splitLine: {
-      show: false
-    }
-  },
-  color:colorList,
-  yAxis: {
-    type: 'value',
-    name: 'km/s minus 299,000',
-    nameTextStyle:{
-      align:'left',
-    },
-    splitArea: {
-      show: false
-    }
-  },
-  series: [
-    {
-      name: 'boxplot',
-      type: 'boxplot',
-      data: boxMustdata2.boxData,
-      tooltip: {
-        formatter: function (param) {
-          return [
-            'Experiment ' + param.name + ': ',
-            'upper: ' + param.data[5],
-            'Q3: ' + param.data[4],
-            'median: ' + param.data[3],
-            'Q1: ' + param.data[2],
-            'lower: ' + param.data[1]
-          ].join('<br/>');
-        }
-      }
-    },
-    {
-      name: 'outlier',
-      type: 'scatter',
-      data: boxMustdata2.outliers
-    }
-  ]
-};
+  seriesData: [{name: "boxplot", type: "boxplot", data: boxMustdata2.boxData},
+    {name: 'outlier', type: 'scatter', data: boxMustdata2.outliers}],
+}
+let boxMustSecond = new BoxMustEchart(GetOpiton(boxMustTwoData, boxMustSecondOption()));
 
-//
-var boxMustTwo = document.getElementById('boxMustSecond');
-var boxMustSecond = echarts.init(boxMustTwo);
-boxMustSecond.setOption(boxMustSecondOption);
+
