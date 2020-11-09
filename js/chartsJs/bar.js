2851cecadd2417b539218d1efbece3e368ca4633
William <1072231718@qq.com>
2020-11-09 17:57:28 +0800

    条形图

--- js/chartsJs/bar.js
+++ js/chartsJs/bar.js
@@ -1,5 +1,5 @@
 /**
- * 条形图类 && 柱状图类
+ * 条形图类 && 柱状图类 && 折线图
  * @opt 图表的option
  * @fn  图表的点击方法
  **/
@@ -8,16 +8,22 @@ class BarEchart extends Echarts {
     super(opt, fn); //在this之前调用super，实际上就是调用父类的构造函数
     this.defaultBar = {
       grid: {
-        left: '0%',
-        right: '0%',
-        bottom: '0%',
-        top: '0%',
         containLabel: true
+      },
+      tooltip:{
+        trigger: 'axis', // 坐标轴指示器，坐标轴触发有效
+        axisPointer: {
+          type: 'shadow',  // 默认为直线，可选为：'line' | 'shadow'
+          textStyle: {
+            color: textColor
+          }
+        },
       },
       xAxis: {
         name: '',
         axisLabel: {
           show: true,
+          interval: 0, //设置为 1，表示『隔一个标签显示一个标签』
           textStyle: {
             fontFamily: 'Microsoft YaHei'
           }
@@ -131,7 +137,7 @@ let barChartOption = () => {
   return option
 };
 
-// 特殊条形图2
+// 胶囊条形图
 let barChartTwoOption = (id) => {
   const option = {
     id: id,
@@ -199,7 +205,6 @@ let barChartTwoOption = (id) => {
       axisTick: {
         show: false
       },
-
     }],
     series: [{
       name: '',
@@ -242,7 +247,7 @@ let barChartTwoOption = (id) => {
   };
   return option;
 }
-// 特殊条形图3
+// 温度条形图
 let barChartThreeOption = (id) => {
   const option = {
     id: id,
@@ -250,10 +255,11 @@ let barChartThreeOption = (id) => {
       left: '8%',
       top: '5%',
       right: '5%',
+      bottom:'0%',
     },
-    xAxis: [{
+    xAxis: {
       show: false,
-    }],
+    },
     yAxis: [{
       axisTick: 'none',
       axisLine: 'none',
@@ -377,15 +383,15 @@ let barChartOneData = {
   seriesData: [{name: '', type: 'bar', data: [120, 150, 180, 200, 220, 250, 300]}]
 }
 
-// 特殊条形图2数据
+// 胶囊条形图数据
 let barChartTwoData = {
   id: 'barChartSecond',
-  yAxis: [['太原市残联', '太原市报社', '太原市民政局', '太原市运输局', '太原市司法局'], [683, 234, 234, 523, 345]],
+  yAxis: [['交通局', '保安局', '民政局', '运输局', '司法局'], [683, 234, 234, 523, 345]],
   seriesData: [{name: '条', type: 'bar', data: [70, 34, 60, 78, 69]},
     {name: '框', type: 'bar', data: [100, 100, 100, 100, 100]}]
 }
 
-// 特殊条形图3数据
+// 温度条形图数据
 let barChartThreeData = {
   id: 'barChartThird',
   yAxis: [['物理', '生物', '化学', '英语', '数学', '语文'], ['6', '5', '4', '3', '2', '1']],
@@ -410,12 +416,11 @@ let barChartFourData = {
 // 条形堆积图数据
 let barChartFiveData = {
   id: 'barChartFifth',
-  legend: ['猪肉类', '牛羊肉类', '鸡鸭类', '三文鱼及产品', '其他水产品'],
+  legend: ['猪肉类', '牛羊肉类', '鸡鸭类'],
   yAxis: ['陕西', '山西', '四川', '北京', '河北', '甘肃', '内蒙'],
   seriesData: [{name: '猪肉类', type: 'bar', data: [320, 302, 301, 334, 390, 330, 120]},
     {name: '牛羊肉类', type: 'bar', data: [120, 132, 101, 134, 90, 230, 210]},
     {name: '鸡鸭类', type: 'bar', data: [220, 182, 191, 234, 290, 330, 310]},
-    {name: '三文鱼及产品', type: 'bar', data: [150, 212, 201, 154, 190, 330, 410]},
   ]
 }
 
@@ -429,296 +434,26 @@ let barChartSevenData = {
   ]
 }
 
-// 双向条形图
-/**
- * 照着一个案例改的
- * 增加下面的数值显示
- * */
-var myData = ['一审服', '撤诉率', '调解率', '实际', '裁判率', '执行标', '再审']
-var barChartSixData = [100, 100, 100, 100, 100, 100, 100]
-var lastYearData = {
-  1: [23, 35, 62, 34, 55, 65, 33]
-}
-var thisYearData = {
-  1: [21, 38, 23, 39, 66, 66, 79]
-}
-var timeLineData = [1]
-
-let barChartSixOption = {
-  id: 'barChartSixth',
-  baseOption: {
-    timeline: {
-      show: false,
-      top: 0,
-      data: []
-    },
-    legend: {
-      left: "center",
-      itemWidth: 15,
-      itemHeight: 10,
-      // icon: 'horizontal',
-      data: ['2017', '2018']
-    },
-    grid: [{
-      show: false,
-      left: '3%',
-      bottom: '2%',
-      top: '10%',
-      containLabel: true,
-      width: '37%'
-    }, {
-      show: false,
-      left: '51%',
-      bottom: '5%',
-      top: '8%',
-      width: '0%'
-    }, {
-      show: false,
-      right: '3%',
-      bottom: '2%',
-      top: '10%',
-      containLabel: true,
-      width: '37%'
-    }],
-    xAxis: [{
-      type: 'value',
-      inverse: true,
-      axisLine: {
-        show: true
-      },
-      axisTick: {
-        show: false
-      },
-      position: 'bottom',
-      axisLabel: {
-        show: true,
-        textStyle: {
-          color: textColor, //X轴左侧字体设置
-          fontSize: 12,
-        }
-      },
-      splitLine: {
-        show: true
-      }
-    }, {
-      gridIndex: 1,
-      show: false,
-    }, {
-      gridIndex: 2,
-      axisLine: {
-        show: true
-      },
-      axisTick: {
-        show: false
-      },
-      position: 'bottom',
-      axisLabel: {
-        show: true,
-        interval: 0,
-        textStyle: {
-          color: textColor, //X轴右侧字体设置
-          fontSize: 12,
-        }
-      },
-      splitLine: {
-        show: true
-      }
-    }],
-    yAxis: [{
-      type: 'category',
-      inverse: true,
-      position: 'right',
-      axisLine: {
-        show: true
-      },
-      axisTick: {
-        show: false
-      },
-      axisLabel: {
-        show: false
-      },
-      data: myData
-    }, {
-      gridIndex: 1,
-      type: 'category',
-      inverse: true,
-      position: 'left',
-      axisLine: {
-        show: false
-      },
-      axisTick: {
-        show: false
-      },
-      axisLabel: {
-        show: true,
-        textStyle: {
-          color: textColor, //中间字体设置
-          fontSize: 12,
-        },
-      },
-      splitNumber: 8,
-      data: myData.map(function (value) {
-        return {
-          value: value,
-          textStyle: {
-            align: 'center',
-          }
-        }
-      })
-    }, {
-      gridIndex: 2,
-      type: 'category',
-      inverse: true,
-      position: 'left',
-      axisLine: {
-        show: true
-      },
-      axisTick: {
-        show: false
-      },
-      axisLabel: {
-        show: false
-      },
-      data: myData
-    }],
-    series: []
-  },
-  options: []
-}
-barChartSixOption.baseOption.timeline.data.push(timeLineData[0])
-barChartSixOption.options.push({
-  series: [{
-    type: 'pictorialBar',
-    xAxisIndex: 0,
-    yAxisIndex: 0,
-    itemStyle: {
-      normal: {
-        color: 'rgba(0,0,0,0)'
-      }
-    },
-    barWidth: 10,
-    data: barChartSixData,
-    barGap: '-100%',
-    barCategoryGap: 0,
-    label: {
-      normal: {
-        show: true,
-        formatter: (series) => {
-          return lastYearData[timeLineData[0]][series.dataIndex] + '%'
-        },
-        position: 'insideTopLeft',
-        textStyle: {
-          color: textColor, //左侧字体设置
-          fontSize: 12,
-        },
-        offset: [0, -5],
-      }
-    },
-    z: -100,
-    animationEasing: 'elasticOut',
-    animationDelay: function (idx) {
-      return idx * 600;
-    }
-  }, {
-    name: '2017',
-    type: 'bar',
-    xAxisIndex: 0,
-    yAxisIndex: 0,
-    //  symbol: 'rect',
-    barWidth: 10,
-    itemStyle: {
-      normal: {
-        barBorderRadius: 5,
-        color: colorList[0]
-      }
-    },
-    // symbolRepeat: true,
-    // symbolSize: 14,
-    data: lastYearData[timeLineData[0]],
-    animationEasing: 'elasticOut',
-    animationDelay: function (idx) {
-      return idx * 600;
-    }
-  },
-    {
-      type: 'pictorialBar',
-      xAxisIndex: 2,
-      yAxisIndex: 2,
-      symbol: 'rect',
-      itemStyle: {
-        normal: {
-          color: 'rgba(0,0,0,0)'
-        }
-      },
-      barWidth: 10,
-      symbolRepeat: true,
-      symbolSize: 14,
-      data: barChartSixData,
-      barGap: '-100%',
-      barCategoryGap: 0,
-      label: {
-        normal: {
-          show: true,
-          formatter: (series) => {
-            return thisYearData[timeLineData[0]][series.dataIndex] + '%'
-          },
-          position: 'insideTopRight',
-          textStyle: {
-            color: textColor, //右侧字体设置
-            fontSize: 12,
-          },
-          offset: [0, -5],
-        }
-      },
-      z: -100,
-      animationEasing: 'elasticOut',
-      animationDelay: function (idx) {
-        return idx * 600;
-      }
-    }, {
-      name: '2018',
-      type: 'bar',
-      xAxisIndex: 2,
-      yAxisIndex: 2,
-      //   symbol: 'rect',
-      barWidth: 10,
-      itemStyle: {
-        normal: {
-          barBorderRadius: 5,
-          color: colorList[1]
-        }
-      },
-      // symbolRepeat: true,
-      // symbolSize: 14,
-      data: thisYearData[timeLineData[0]],
-      animationEasing: 'elasticOut',
-      animationDelay: function (idx) {
-        return idx * 600;
-      }
-    }
-  ]
-});
 
 
 // 条形图
-let barOneData = dataFormat(barChartOneData, '条形图1');
+let barOneData = dataFormat(barChartOneData, 'bar1');
 var barChartFirst = new BarEchart(GetOpiton(barOneData, barChartOption()));
 
 // 条形图2
 var barChartSecond = new BarEchart(GetOpiton(barChartTwoData, barChartTwoOption()));
-// 条形图3
+// 温度条形图
 var barChartThird = new BarEchart(GetOpiton(barChartThreeData, barChartThreeOption()));
 // 条形多柱图
-let barFourData = dataFormat(barChartFourData, '条形图');
+let barFourData = dataFormat(barChartFourData, 'bar');
 var barChartFourth = new BarEchart(GetOpiton(barFourData, barChartOption()));
 
 // 条形堆积图
-let barFiveData = dataFormat(barChartFiveData, '条形堆积图');
+let barFiveData = dataFormat(barChartFiveData, 'barStack');
 var barChartFifth = new BarEchart(GetOpiton(barFiveData, barChartOption()));
 
 // 正负条形图
-let barSevenData = dataFormat(barChartSevenData, '正负条形图');
+let barSevenData = dataFormat(barChartSevenData, 'barPlusMinus');
 var barChartSeventh = new BarEchart(GetOpiton(barSevenData, barChartOption()));
 
-// 双向条形图
-var barChartSixth = new BarEchart(barChartSixOption);
+
