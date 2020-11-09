9775b09125e15fccc274b65cc96f0a7d69318ba2
William <1072231718@qq.com>
2020-11-09 17:57:50 +0800

    柱状图和3d立体柱状图

--- js/chartsJs/histogram.js
+++ js/chartsJs/histogram.js
@@ -1,4 +1,3 @@
-
 /*柱状图模板Option
 * @{柱状图, 多柱图 ,堆积柱图}
 * */
@@ -139,7 +138,7 @@ let histogramFiveOption = () => {
       axisLabel: {
         color: textColor,
         margin: 2,
-        interval:0,
+        interval: 0,
       },
       axisLine: {
         lineStyle: {
@@ -237,7 +236,9 @@ let histogramFiveOption = () => {
       barWidth: '25%',
       itemStyle: {
         normal: {
-          color: (params => {return colorList[params.seriesIndex % colorList.length]})
+          color: (params => {
+            return colorList[params.seriesIndex % colorList.length]
+          })
         }
       },
       xAxisIndex: 1,
@@ -249,14 +250,14 @@ let histogramFiveOption = () => {
 
 // ----柱状图数据----------
 let histogramOneData = {
-  id:'histogramFirst',
+  id: 'histogramFirst',
   xAxis: ['制造业', '建筑业', '农林牧渔', '房地产', '金融业'],
   seriesData: [{name: '2019', type: 'bar', data: [5000, 2600, 1300, 1300, 1250]}]
 }
 
 // ------多系列柱图数据------------
 let histogramTwoData = {
-  id:'histogramSecond',
+  id: 'histogramSecond',
   legend: ['健康度', '可用度'],
   xAxis: ['22:18', '22:23', '22:25', '22:28', '22:30', '22:33', '22:35'],
   seriesData: [{name: '健康度', type: 'bar', data: [20, 25, 40, 55, 65, 70, 80]},
@@ -265,33 +266,31 @@ let histogramTwoData = {
 
 // ------堆积柱图数据------------
 var histogramThreeData = {
-  id:'histogramThird',
-  legend: ['因病', '因残', '因学', '因灾', '缺水',],
-  xAxis: ['新荣区', '平城区', '云冈区', '云州区'],
+  id: 'histogramThird',
+  legend: ['智能环空设备', '智能饲喂器', '智能摄像头'],
+  xAxis: ['一期', '二期', '三期', '四期'],
   seriesData: [
-    {name: '因病', type: 'bar', data: [520, 502, 601, 534]},
-    {name: '因残', type: 'bar', data: [520, 602, 701, 434]},
-    {name: '因学', type: 'bar', data: [601, 402, 501, 500]},
-    {name: '因灾', type: 'bar', data: [601, 702, 401, 634]},
-    {name: '缺水', type: 'bar', data: [520, 601, 601, 434]},
+    {name: '智能环空设备', type: 'bar', data: [520, 502, 601, 534]},
+    {name: '智能饲喂器', type: 'bar', data: [520, 602, 701, 434]},
+    {name: '智能摄像头', type: 'bar', data: [601, 402, 501, 500]},
   ]
 }
 
 // 阶梯瀑布图
 let hisogramFourData = {
-  id:'histogramFourth',
+  id: 'histogramFourth',
   legend: ['支出', '收入'],
-  xAxis: ["11月1日", "11月2日", "11月3日", "11月4日", "11月5日", "11月6日", "11月7日", "11月8日", "11月9日", "11月10日", "11月11日"],
+  xAxis: ["11月1日", "11月2日", "11月3日", "11月4日", "11月5日", "11月6日", "11月7日", "11月8日", "11月9日"],
   seriesData: [
-    {name: '辅助', type: 'bar', data: [0, 900, 1245, 1530, 1376, 1376, 1511, 1689, 1856, 1495, 1292]},
-    {name: '收入', type: 'bar', data: [900, 345, 393, '', '', 500, 400, 286, '', '', '']},
-    {name: '支出', type: 'bar', data: ['', '', '', 300, 400, '', '', '', 550, 361, 503]},
+    {name: '辅助', type: 'bar', data: [0, 900, 1245, 1530, 1376, 1376, 1511, 1856, 1495]},
+    {name: '收入', type: 'bar', data: [900, 345, 393, '', '', 500, 400, '', '']},
+    {name: '支出', type: 'bar', data: ['', '', '', 300, 400, '', '', 550, 361]},
   ]
 }
 
 // 多系列柱图
 let histogramFiveData = {
-  id:'histogramFifth',
+  id: 'histogramFifth',
   legend: ['均值', '峰值'],
   xAxis: [['cpu', '内存', '磁盘', 'cpu', '内存', '磁盘', 'cpu', '内存', '磁盘', 'cpu', '内存', '磁盘']],
   seriesData: [
@@ -304,14 +303,70 @@ let histogramFiveData = {
   ]
 }
 
+// 3d柱状图
+var histogramSixData = {
+  id: 'histogramSixth',
+  xAxis: ['数据可视化', '商业智能', '区块链', '数据挖掘', '人工智能'],
+  seriesData: [{name: '2019', type: 'bar', data: [1000, 600, 800, 700, 550], symbol: "circle"}]
+}
+
+//3d柱状图 - 波纹ripple
+var histogramSevenData = {
+  id: 'histogramSeventh',
+  xAxis: ['制造业', '建筑业', '农林牧渔', '房地产', '金融业'],
+  seriesData: [{name: '2019', type: 'bar', data: [4000, 2600, 1300, 1300, 1250], symbol: "circle", target: 3500}]
+}
+
+//3d柱状图 - 阴影shadow
+var histogramEightData = {
+  id: 'histogramEighth',
+  xAxis: ['数据结构', '高等数学', '线性代数', 'Java课程', '3dMax'],
+  seriesData: [{name: '在线情况', type: 'bar', data: [500, 300, 200, 350, 250], symbol: "circle", max: 500}]
+}
+
+//3d柱状图 - 方形
+var histogramNineData = {
+  id: 'histogramNinth',
+  xAxis: ['制造业', '建筑业', '农林牧渔', '房地产', '金融业'],
+  seriesData: [{name: '2019', type: 'bar', data: [4000, 2600, 1300, 1300, 1250], symbol: "diamond"}]
+}
+
+//3d柱状图 - 方形阴影
+var histogramTenData = {
+  id: 'histogramTenth',
+  xAxis: ['立春', '春分', '立夏', '夏至'],
+  seriesData: [{name: '在线情况', type: 'bar', data: [200, 300, 150, 120], symbol: "diamond", max: 300}]
+}
+
+
 // 柱状图
 var histogramFirst = new BarEchart(GetOpiton(histogramOneData, histogramOption()));
 // 多柱图
 var histogramSecond = new BarEchart(GetOpiton(histogramTwoData, histogramOption()));
 // 堆积柱图
-var histogramThree_data = dataFormat(histogramThreeData,'堆积柱图')
+var histogramThree_data = dataFormat(histogramThreeData, 'columnarStack')
 var histogramThird = new BarEchart(GetOpiton(histogramThree_data, histogramOption()));
 // 阶梯瀑布图
 var histogramFourth = new BarEchart(GetOpiton(hisogramFourData, histogramLadderOption()));
 // 多系列柱图
 var histogramFifth = new BarEchart(GetOpiton(histogramFiveData, histogramFiveOption()));
+// 3d柱状图
+let histogramSix_data = dataFormat(histogramSixData, 'columnar3d')
+let histogramSixth = new BarEchart(GetOpiton(histogramSix_data, histogramOption()));
+
+//3d柱状图 - 波纹ripple
+let histogramSeven_data = dataFormat(histogramSevenData, 'columnar3d_ripple')
+let histogramSeventh = new BarEchart(GetOpiton(histogramSeven_data, histogramOption()));
+
+//3d柱状图 - 阴影shadow
+let histogramEight_data = dataFormat(histogramEightData, 'columnar3d_shadow')
+let histogramEighth = new BarEchart(GetOpiton(histogramEight_data, histogramOption()));
+histogramEighth.showOption()
+
+//3d柱状图 - 方形
+let histogramNinth_data = dataFormat(histogramNineData, 'columnar3d')
+let histogramNinth = new BarEchart(GetOpiton(histogramNinth_data, histogramOption()));
+
+//3d柱状图 - 方形阴影
+let histogramTenth_data = dataFormat(histogramTenData, 'columnar3d_shadow')
+let histogramTenth = new BarEchart(GetOpiton(histogramTenth_data, histogramOption()));
