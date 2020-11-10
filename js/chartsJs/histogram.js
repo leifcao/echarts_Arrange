e39200c6787732f2a3f54fecb82390ccd6aafca7
William <1072231718@qq.com>
2020-11-10 15:50:14 +0800

    柱状图和3d立体柱状和阶梯瀑布

--- js/chartsJs/histogram.js
+++ js/chartsJs/histogram.js
@@ -1,7 +1,7 @@
 /*柱状图模板Option
-* @{柱状图, 多柱图 ,堆积柱图}
+* @{柱状图, 多柱图 ,堆积柱图, 阶梯瀑布图}
 * */
-let histogramOption = (type) => {
+let histogramOption = () => {
   const option = {
     legend: {
       data: [],
@@ -22,6 +22,11 @@ let histogramOption = (type) => {
     yAxis: {
       name: '', //单位：万元
       type: 'value',
+      splitLine: {
+        lineStyle: {
+          opacity: 0.4
+        }
+      },
       axisTick: {
         show: false
       },
@@ -50,203 +55,6 @@ let histogramOption = (type) => {
   };
   return option;
 }
-
-// 阶梯瀑布图 模板
-let histogramLadderOption = () => {
-  const option = {
-    tooltip: {},
-    legend: {
-      data: [],
-    },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      top: '11%',
-    },
-    xAxis: {
-      type: 'category',
-      splitLine: {show: false},
-      boundaryGap: true,
-      data: []
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        name: '',
-        type: 'bar',
-        stack: '总量',
-        label: {
-          show: true,
-          position: 'top'
-        },
-        barWidth: '10',
-        itemStyle: {
-          normal: {
-            color: '',
-            barBorderRadius: 12,
-          },
-        },
-        data: []
-      },
-      {
-        name: '辅助',
-        type: 'bar',
-        stack: '总量',
-        itemStyle: {
-          barBorderColor: 'rgba(0,0,0,0)',
-          color: 'rgba(0,0,0,0)'
-        },
-        emphasis: {
-          itemStyle: {
-            barBorderColor: 'rgba(0,0,0,0)',
-            color: 'rgba(0,0,0,0)'
-          }
-        },
-        data: []
-      },
-    ]
-  }
-  return option;
-};
-
-// 多系列柱图 模板
-let histogramFiveOption = () => {
-  const option = {
-    legend: {
-      right: 'center',
-      data: []
-    },
-    grid: [
-      {
-        top: '10%',
-        bottom: '15%',
-        right: '5%',
-      },
-      {
-        height: '15%',
-        right: '5%',
-        bottom: '0%'
-      }
-    ],
-    xAxis: [{
-      type: 'category',
-      data: [],
-      gridIndex: 0,
-      axisLabel: {
-        color: textColor,
-        margin: 2,
-        interval: 0,
-      },
-      axisLine: {
-        lineStyle: {
-          color: textColor
-        }
-      },
-      axisTick: {
-        lineStyle: {
-          color: textColor
-        }
-      },
-      zlevel: 2
-    }, {
-      type: 'category',
-      gridIndex: 1,
-      axisLine: {
-        show: false
-      },
-      zlevel: 1
-    }],
-    yAxis: [{
-      type: 'value',
-      gridIndex: 0,
-      axisLabel: {
-        color: '#333'
-      },
-      splitLine: {
-        lineStyle: {
-          type: 'dashed'
-        }
-      },
-      axisLine: {
-        lineStyle: {
-          color: textColor
-        }
-      },
-      axisTick: {
-        lineStyle: {
-          color: textColor
-        }
-      }
-    }, {
-      type: 'value',
-      gridIndex: 1,
-      axisLabel: {
-        show: false
-      },
-      axisLine: {
-        show: false
-      },
-      splitLine: {
-        show: false
-      },
-      axisTick: {
-        show: false
-      }
-    }],
-    series: [{
-      name: '',
-      data: [],
-      type: 'bar',
-      label: {
-        show: true,
-        position: 'top',
-        textStyle: {
-          color: textColor
-        }
-      },
-      itemStyle: {
-        normal: {
-          color: (params) => {
-            let len = colorList.length;
-            let colors = params.seriesIndex === 0 ? colorList.slice(0, 3) : colorList.slice(len - 3, len)
-            return colors[params.dataIndex % colors.length]
-          },
-          barBorderRadius: 10,
-        }
-      },
-      xAxisIndex: 0,
-      yAxisIndex: 0
-    }, {
-      name: '电脑',
-      data: [],
-      label: {
-        show: true,
-        position: 'inside',
-        formatter: '{b}',
-        offset: [0, 10],
-        textStyle: {
-          color: '#777'
-        }
-      },
-      type: 'bar',
-      barGap: 0,
-      barWidth: '25%',
-      itemStyle: {
-        normal: {
-          color: (params => {
-            return colorList[params.seriesIndex % colorList.length]
-          })
-        }
-      },
-      xAxisIndex: 1,
-      yAxisIndex: 1
-    }]
-  }
-  return option;
-};
 
 // ----柱状图数据----------
 let histogramOneData = {
@@ -280,62 +88,27 @@ var histogramThreeData = {
 let hisogramFourData = {
   id: 'histogramFourth',
   legend: ['支出', '收入'],
-  xAxis: ["11月1日", "11月2日", "11月3日", "11月4日", "11月5日", "11月6日", "11月7日", "11月8日", "11月9日"],
+  xAxis: ["11月1日", "11月2日", "11月3日", "11月4日", "11月5日", "11月6日", "11月7日", "11月8日"],
   seriesData: [
-    {name: '辅助', type: 'bar', data: [0, 900, 1245, 1530, 1376, 1376, 1511, 1856, 1495]},
-    {name: '收入', type: 'bar', data: [900, 345, 393, '', '', 500, 400, '', '']},
-    {name: '支出', type: 'bar', data: ['', '', '', 300, 400, '', '', 550, 361]},
+    {name: '辅助', type: 'bar', data: [0, 900, 1245, 1530, 1376, 1376, 1511, 1856]},
+    {name: '收入', type: 'bar', data: [900, 345, 393, '', '', 500, 400, '',]},
+    {name: '支出', type: 'bar', data: ['', '', '', 300, 400, '', '', 550,]},
   ]
 }
 
-// 多系列柱图
-let histogramFiveData = {
-  id: 'histogramFifth',
-  legend: ['均值', '峰值'],
-  xAxis: [['cpu', '内存', '磁盘', 'cpu', '内存', '磁盘', 'cpu', '内存', '磁盘', 'cpu', '内存', '磁盘']],
-  seriesData: [
-    {name: '均值', type: 'bar', data: [10, 15, 18, 25, 23, 32, 33, 36, 57, 72, 45, 45]},
-    {name: '峰值', type: 'bar', data: [20, 50, 80, 50, 30, 50, 60, 60, 70, 90, 50, 50]},
-    {name: '电脑', type: 'bar', data: [{name: '联想', value: 1}]},
-    {name: '电脑', type: 'bar', data: [{name: '华为', value: 1}]},
-    {name: '电脑', type: 'bar', data: [{name: '华硕', value: 1}]},
-    {name: '电脑', type: 'bar', data: [{name: '苹果', value: 1}]},
-  ]
-}
-
-// 3d柱状图
-var histogramSixData = {
-  id: 'histogramSixth',
-  xAxis: ['数据可视化', '商业智能', '区块链', '数据挖掘', '人工智能'],
-  seriesData: [{name: '2019', type: 'bar', data: [1000, 600, 800, 700, 550], symbol: "circle"}]
-}
-
-//3d柱状图 - 波纹ripple
-var histogramSevenData = {
-  id: 'histogramSeventh',
-  xAxis: ['制造业', '建筑业', '农林牧渔', '房地产', '金融业'],
-  seriesData: [{name: '2019', type: 'bar', data: [4000, 2600, 1300, 1300, 1250], symbol: "circle", target: 3500}]
-}
 
 //3d柱状图 - 阴影shadow
 var histogramEightData = {
   id: 'histogramEighth',
   xAxis: ['数据结构', '高等数学', '线性代数', 'Java课程', '3dMax'],
-  seriesData: [{name: '在线情况', type: 'bar', data: [500, 300, 200, 350, 250], symbol: "circle", max: 500}]
-}
-
-//3d柱状图 - 方形
-var histogramNineData = {
-  id: 'histogramNinth',
-  xAxis: ['制造业', '建筑业', '农林牧渔', '房地产', '金融业'],
-  seriesData: [{name: '2019', type: 'bar', data: [4000, 2600, 1300, 1300, 1250], symbol: "diamond"}]
+  seriesData: [{name: '在线情况', type: 'bar', data: [400, 300, 200, 350, 250], symbol: "circle", max: 500}]
 }
 
 //3d柱状图 - 方形阴影
 var histogramTenData = {
   id: 'histogramTenth',
   xAxis: ['立春', '春分', '立夏', '夏至'],
-  seriesData: [{name: '在线情况', type: 'bar', data: [200, 300, 150, 120], symbol: "diamond", max: 300}]
+  seriesData: [{name: '在线情况', type: 'bar', data: [200, 250, 150, 120], symbol: "diamond", max: 300}]
 }
 
 
@@ -347,25 +120,14 @@ var histogramSecond = new BarEchart(GetOpiton(histogramTwoData, histogramOption(
 var histogramThree_data = dataFormat(histogramThreeData, 'columnarStack')
 var histogramThird = new BarEchart(GetOpiton(histogramThree_data, histogramOption()));
 // 阶梯瀑布图
-var histogramFourth = new BarEchart(GetOpiton(hisogramFourData, histogramLadderOption()));
-// 多系列柱图
-var histogramFifth = new BarEchart(GetOpiton(histogramFiveData, histogramFiveOption()));
-// 3d柱状图
-let histogramSix_data = dataFormat(histogramSixData, 'columnar3d')
-let histogramSixth = new BarEchart(GetOpiton(histogramSix_data, histogramOption()));
-
-//3d柱状图 - 波纹ripple
-let histogramSeven_data = dataFormat(histogramSevenData, 'columnar3d_ripple')
-let histogramSeventh = new BarEchart(GetOpiton(histogramSeven_data, histogramOption()));
+var hisogramFour_data = dataFormat(hisogramFourData, 'columnarLadder');
+var histogramFourth = new BarEchart(GetOpiton(hisogramFour_data, histogramOption()));
+histogramFourth.showOption()
 
 //3d柱状图 - 阴影shadow
 let histogramEight_data = dataFormat(histogramEightData, 'columnar3d_shadow')
 let histogramEighth = new BarEchart(GetOpiton(histogramEight_data, histogramOption()));
-histogramEighth.showOption()
-
-//3d柱状图 - 方形
-let histogramNinth_data = dataFormat(histogramNineData, 'columnar3d')
-let histogramNinth = new BarEchart(GetOpiton(histogramNinth_data, histogramOption()));
+// histogramEighth.showOption()
 
 //3d柱状图 - 方形阴影
 let histogramTenth_data = dataFormat(histogramTenData, 'columnar3d_shadow')
