e7d740fbb572d6e51493afb0cc6083481711cc3d
William <1072231718@qq.com>
2020-11-20 15:17:14 +0800

    窗口自适应

--- js/resize.js
+++ js/resize.js
@@ -1,26 +1,8 @@
  // 自适应
  window.onresize = function() {
      resize(Echarts_List);
-     // 迁徙图
-     mapMigrateEchart.resize();
-     // 世界迁徙图
-     mapImmigrateEchart.resize();
-     // 气泡地图
-     mapBubbleEchart.resize();
-     // 轮播地图
-     mapCarouselEchart.resize();
-     //大区下钻地图s
+     //大区下钻地图
      mapRegionEchart.resize();
      // 中国下钻地图
      mapUnderEchart.resize();
-
-     //K线图
-     k_lineFirst.resize();
-     k_lineSecond.resize();
-
-
-     //树图
-     horizontalTreeEchart.resize();
-     verticalTreeEchart.resize();
-
  }
