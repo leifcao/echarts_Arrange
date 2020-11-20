4c6a40c7e3a173bd354060cdcb32bebb0e3d808a
William <1072231718@qq.com>
2020-11-20 15:19:54 +0800

    中国各大区下钻

--- js/chartsJs/map/mapRegion.js
+++ js/chartsJs/map/mapRegion.js
@@ -5,157 +5,177 @@
 var allprovinceData = [{
   name: "黑龙江省",
   value: 4,
-  },
+  region: '东北大区'
+},
   {
     name: "吉林省",
     value: 4,
+    region: '东北大区'
   },
   {
     name: "辽宁省",
     value: 4,
+    region: '东北大区'
   },
   {
     name: "北京市",
     value: 4,
+    region: '东北大区'
   },
   {
     name: "天津省",
     value: 4,
+    region: '东北大区'
   },
   {
     name: "河北省",
     value: 4,
+    region: '东北大区'
   },
   {
     name: "内蒙古自治区",
     value: 4,
+    region: '东北大区'
   },
   {
     name: "上海市",
     value: 2,
+    region: '中部大区'
   },
   {
     name: "江苏省",
     value: 2,
+    region: '中部大区'
   },
   {
     name: "安徽省",
     value: 2,
+    region: '中部大区'
   },
   {
     name: "山东省",
     value: 2,
+    region: '中部大区'
   },
   {
     name: "河南省",
     value: 2,
+    region: '中部大区'
   },
   {
     name: "湖北省",
     value: 2,
+    region: '中部大区'
   },
   {
     name: "浙江省",
-    value: 11,
+    value: 5,
+    region: '东南大区'
   },
   {
     name: "福建省",
-    value: 11,
+    value: 5,
+    region: '东南大区'
   },
   {
     name: "湖南省",
-    value: 11,
+    value: 5,
+    region: '东南大区'
   },
   {
     name: "江西省",
-    value: 11,
+    value: 5,
+    region: '东南大区'
   },
   {
     name: "台湾省",
-    value: 11,
+    value: 5,
+    region: '东南大区'
   },
   {
     name: "重庆市",
-    value: 11,
+    value: 5,
+    region: '东南大区'
   },
   {
     name: "广东省",
-    value: 0,
+    value: 3,
+    region: '西南大区'
   },
   {
     name: "广西壮族自治区",
-    value: 0,
+    value: 3,
+    region: '西南大区'
   },
   {
     name: "海南省",
-    value: 0,
+    value: 3,
+    region: '西南大区'
   },
   {
     name: "香港特别行政区",
-    value: 0,
+    value: 3,
+    region: '西南大区'
   },
   {
     name: "澳门特别行政区",
-    value: 0,
+    value: 3,
+    region: '西南大区'
   },
 
   {
     name: "四川省",
-    value: 0,
+    value: 3,
+    region: '西南大区'
   },
   {
     name: "贵州省",
-    value: 0,
+    value: 3,
+    region: '西南大区'
   },
   {
     name: "云南省",
-    value: 0,
+    value: 3,
+    region: '西南大区'
   },
   {
     name: "西藏自治区",
-    value: 0,
+    value: 3,
+    region: '西南大区'
   },
   {
     name: "山西省",
     value: 9,
+    region: '西北大区'
   },
   {
     name: "陕西省",
     value: 9,
+    region: '西北大区'
   },
   {
     name: "甘肃省",
     value: 9,
+    region: '西北大区'
   },
   {
     name: "青海省",
-    value: 9,
+    value: 11,
+    region: '西北大区'
   },
   {
     name: "宁夏回族自治区",
     value: 9,
+    region: '西北大区'
   },
   {
     name: "新疆维吾尔自治区",
     value: 9,
+    region: '西北大区'
   },
 
 ];
-var mapRegion_visualMap ={
-  min: 0,
-  max: 11,
-  left: 'left',
-  top: 'bottom',
-  text: ['高', '低'],
-  calculable: true,
-  seriesIndex: '1',
-  show: false,
-  inRange: {
-    color: ['#479fd2', '#fba853', '#48c7c0', '#fa8737', '#4bbdd6', '#ff6f5b']
-    // color: ['#ffffff', '#ffc188', '#479fd2', '#fba853', '#48c7c0', '#fa8737', '#4bbdd6', '#ff6f5b']
-  }
-};
-
+var mapRegion_visualMap = {};
 
 
 var mapRegionEchart;
@@ -168,12 +188,14 @@ $.getJSON('data/100000.json', function (geoJson) {
     goDown: true, // 是否下钻
     geoJson: geoJson,  // 地图数据
     region: true,   // 是否设置各大区
-    data:allprovinceData,  // 大区区块颜色data
-    visualMap:mapRegion_visualMap, // 大区图例
+    // config: {'backgroundColor': 'red'},
+    data: allprovinceData,  // 大区区块颜色data
+    visualMap: mapRegion_visualMap, // 大区图例
     // 下钻回调
     callback: function (name, option, instance) {
       //console.log(name, option, instance);
     },
   });
-
 })
+
+
